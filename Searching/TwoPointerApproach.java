1a41499b4f21fe95b385716ab1d2189981298dab
Vishal Singh <v.singh.kld@gmail.com>
2020-05-19 11:23:32 +0530

    Updated array

--- Searching/TwoPointerApproach.java
+++ Searching/TwoPointerApproach.java
@@ -1,5 +1,7 @@
 package SummerTrainingGFG.Searching;
 
+import java.util.HashMap;
+
 /**
  * Given a sorted array and a sum find if there is a pair with given sum*/
 class PairSum{
@@ -22,14 +24,46 @@ class PairSum{
         return false;
     }
 }
+/*
+* Find triplet in an array which gives sum X*/
 
+/*
+class TripletSum{
+    boolean isPair(int arr[], int left,int right,int sum){
+        while (left < right) {
+            if (arr[left] + arr[left] + arr[right] == sum)
+                return true;
+            else if (arr[left] + arr[left] + arr[right] < sum)
+                left++;
+            else
+                right--;
+        }
+        return false;
+    }
+    boolean findTripletSumExistence(int[] arr,int sum){
+        int n = arr.length;
+        for (int i = 0; i < n ; i++) {
+            if (isPair(arr,i+1,n-1,sum)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+*/
 public class TwoPointerApproach {
     public static void main(String[] args) {
-        PairSum pairSum = new PairSum();
         int arr[] = {
                 2,4,8,9,11,12,20,30
         };
         int sum = 23;
+
+        PairSum pairSum = new PairSum();
         System.out.println(pairSum.findPairSumExistence(arr,sum));
+/*
+        TripletSum tripletSum = new TripletSum();
+        System.out.println(tripletSum.findTripletSumExistence(arr,14));
+*/
+
     }
 }
