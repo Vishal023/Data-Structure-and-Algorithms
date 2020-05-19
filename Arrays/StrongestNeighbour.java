a403f5edcf920388c6d1946ccc11a5945efea6d9
Vishal Singh <v.singh.kld@gmail.com>
2020-05-19 11:29:37 +0530

    Added strongest neighbour

--- Arrays/StrongestNeighbour.java
+++ Arrays/StrongestNeighbour.java
@@ -1,4 +1,18 @@
 package SummerTrainingGFG.Arrays;
-
 public class StrongestNeighbour {
+
+    static void maximumAdjacent(int sizeOfArray, int[] arr){
+        /*
+        *******************************
+         * Use string buffer for fast output
+         * ***********************************/
+        StringBuffer s=new StringBuffer();
+        for(int i = 0;i < sizeOfArray-1; i=i+1){
+            s.append(Math.max(arr[i], arr[i + 1])).append(" ");
+        }
+        System.out.print(s);
+    }
+    public static void main(String[] args) {
+        maximumAdjacent(6,new int[]{1,2,2,3,4,5});
+    }
 }
