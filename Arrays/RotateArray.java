1c9f65e346750cbb4225b03dbfff23348c10263a
Vishal Singh <v.singh.kld@gmail.com>
2020-05-19 12:43:02 +0530

    Added RotateArray

---
+++ Arrays/RotateArray.java
@@ -0,0 +1,28 @@
+package SummerTrainingGFG.Arrays;
+
+public class RotateArray {
+    static void reverse(int arr[],int low,int high){
+        int temp = 0;
+        while (low<high){
+            temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+    static void rotateArr(int arr[], int d, int n)
+    {
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+        StringBuffer s = new StringBuffer();
+        for (int i = 0; i <  n; i++) {
+            s.append(arr[i]).append(" ");
+        }
+        System.out.print(s);
+    }
+    public static void main(String[] args) {
+        rotateArr(new int[]{1,2,3,4,5},2,5);
+    }
+}
