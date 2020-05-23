96baf5657f25183cadf5205f5b28d9ce5cfca0d9
Vishal Singh <v.singh.kld@gmail.com>
2020-05-23 20:26:43 +0530

    Updated Get the shadow

--- Contest/GetTheShadow.java
+++ Contest/GetTheShadow.java
@@ -40,23 +40,35 @@ public class GetTheShadow {
         int t = fastReader.nextInt();
         while (t-->0){
             int n = fastReader.nextInt();
-            int A[] = new int[n];
+            int A[] = new int[n+1];
             String[] arrayElements = (fastReader.nextLine()).split(" ");
-            for (int i = 0; i < n; i++) {
-                A[i] = Integer.parseInt(arrayElements[i]);
+            for (int i = 1; i <= n; i++) {
+                A[i] = Integer.parseInt(arrayElements[i-1]);
             }
-            int b;//repeating number
-            int a;//missing number
-            int set[] = new int[n];
+            int b = -1;//repeating number
+            int a = -1;//missing number
+            int set[] = new int[n+1];
             /*
             * yet to do
             * */
-            for (int i = 0; i < n; i++) {
-                set[i] = i+1;
+            for (int i = 1; i <= n; i++) {
+                set[i] = 0;
             }
-            for (int i = 0; i < n ; i++) {
-
+            for (int i = 1; i <= n ; i++) {
+                if (set[A[i]] == 0) {
+                    set[A[i]] = 1;
+                }
+                if (set[A[i]] == 1){
+                    b = A[i];
+                }
             }
+            for (int i = 1; i <= n ; i++) {
+                if (set[i] == 0){
+                    a = i;
+                    break;
+                }
+            }
+            System.out.println(b+" "+a);
         }
     }
 }
