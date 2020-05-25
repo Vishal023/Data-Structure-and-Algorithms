aafe58f2a4f6203960a5e435b5fddea9ca9fac01
Vishal Singh <v.singh.kld@gmail.com>
2020-05-26 00:07:03 +0530

    Max Occured Integer

---
+++ Arrays/MaxOccurredInteger.java
@@ -0,0 +1,34 @@
+package SummerTrainingGFG.Arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * @author Vishal Singh */
+public class MaxOccurredInteger {
+    static int maxOccurredInteger(int[] left, int[] right){
+        int[] arr = new int[1000000];
+        for (int i = 0; i < left.length; i++) {
+            arr[left[i]]+=1;
+            arr[right[i]+1]--;
+        }
+        int maxSum = arr[0];
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i] + arr[i-1];
+            if (maxSum < arr[i]){
+                maxSum = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(maxOccurredInteger(  new int[]{1,2,3},
+                                                new int[]{3,5,7}
+                                                ));
+    }
+}
