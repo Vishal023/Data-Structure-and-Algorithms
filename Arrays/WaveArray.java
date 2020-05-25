0d556f721e92b8a5c8ce6e40e6dd5c03fcc70018
Vishal Singh <v.singh.kld@gmail.com>
2020-05-25 10:15:53 +0530

    Wave array - added question statement

--- Arrays/WaveArray.java
+++ Arrays/WaveArray.java
@@ -3,11 +3,16 @@ package SummerTrainingGFG.Arrays;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Scanner;
+
+/**
+ * @author Vishal Singh */
 
 public class WaveArray {
     public static void main(String[] args)throws IOException {
+        /*
+         * Given a sorted array arr[] of distinct integers. Sort the array into a wave-like array and return it.
+         * In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5..... (considering the increasing lexicographical order).
+         * */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         while(t-- > 0){
