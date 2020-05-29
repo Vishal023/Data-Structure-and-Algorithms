ba6df1378936294bff37e7ae59f978c6cf529b57
Vishal Singh <v.singh.kld@gmail.com>
2020-05-29 22:12:58 +0530

    Updated Two pointer approach

--- Searching/TwoPointerApproach.java
+++ Searching/TwoPointerApproach.java
@@ -3,68 +3,59 @@ package SummerTrainingGFG.Searching;
 /**
  * @author Vishal Singh */
 
-/*
- * Given a sorted array and a sum find if there is a pair with given sum*/
-
 class PairSum{
+
+    /*
+     * Given a sorted array and a sum find if there is a pair with given sum*/
     boolean findPairSumExistence(int[] arr, int sum){
-        int left = 0;
-        int right = arr.length-1;
+        return searchPair(arr,0,arr.length-1,sum);
+    }
+    boolean searchPair(int[]arr, int left,int right,int sum){
         while (left<right){
-            int currSum = arr[left]+arr[right];
-            if (currSum < sum){
-                left++;
+            int currentSum = arr[left]+arr[right];
+            if (currentSum == sum){
+                return true;
             }
-            else if (currSum>sum){
+            if (currentSum>sum){
                 right--;
             }
             else {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-/*
-* Find triplet in an array which gives sum X*/
-
-/*
-class TripletSum{
-    boolean isPair(int arr[], int left,int right,int sum){
-        while (left < right) {
-            if (arr[left] + arr[left] + arr[right] == sum)
-                return true;
-            else if (arr[left] + arr[left] + arr[right] < sum)
                 left++;
-            else
-                right--;
+            }
         }
         return false;
     }
+}
+class TripletSum extends PairSum{
+    /*
+     * Find triplet in an array which gives sum X*/
     boolean findTripletSumExistence(int[] arr,int sum){
-        int n = arr.length;
-        for (int i = 0; i < n ; i++) {
-            if (isPair(arr,i+1,n-1,sum)){
+        for (int i = 0; i < arr.length ; i++) {
+            if(searchPair(arr,i+1,arr.length-1,sum-arr[i])){
+                System.out.println(arr[i]);
                 return true;
             }
         }
         return false;
     }
 }
-*/
+
 public class TwoPointerApproach {
     public static void main(String[] args) {
-        int arr[] = {
+
+        int[] arr = {
                 2,4,8,9,11,12,20,30
         };
-        int sum = 23;
-
+        int sum1 = 23;
+        int sum2 = 7;
         PairSum pairSum = new PairSum();
-        System.out.println(pairSum.findPairSumExistence(arr,sum));
-/*
-        TripletSum tripletSum = new TripletSum();
-        System.out.println(tripletSum.findTripletSumExistence(arr,14));
-*/
+        System.out.println("Pair Sum");
+        System.out.println(pairSum.findPairSumExistence(arr,sum1)+" with sum "+ sum1);
+        System.out.println(pairSum.findPairSumExistence(arr,sum2)+" with sum "+ sum2);
 
+        TripletSum tripletSum = new TripletSum();
+        System.out.println("Triplet Sum");
+        System.out.println(tripletSum.findTripletSumExistence(arr,sum1)+" with sum "+ sum1);
+        System.out.println(tripletSum.findTripletSumExistence(arr,sum2)+" with sum "+ sum2);
     }
 }
