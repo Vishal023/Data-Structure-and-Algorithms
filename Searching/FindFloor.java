564f2562205f8d90b005aab4cd3da93bc856a1d5
Vishal Singh <v.singh.kld@gmail.com>
2020-05-30 15:22:42 +0530

    Updated FindFloor

--- Searching/FindFloor.java
+++ Searching/FindFloor.java
@@ -9,6 +9,9 @@ public class FindFloor {
         if (l>h){
             return -1;
         }
+        if (arr[h] <= element){
+            return h;
+        }
         int mid = l + (h-l)/2;
         if (arr[mid] == element){
             return mid;
@@ -16,17 +19,13 @@ public class FindFloor {
         if (mid > 0 && arr[mid-1] <= element && arr[mid] > element ){
             return mid-1;
         }
-        if (arr[mid] > element){
+        if (element < arr[mid]){
             return find(arr,l,mid-1,element);
         }
-        if (arr[mid] < element){
-            return find(arr,mid+1,h,element);
-        }
-        return -1;
+        return find(arr,mid+1,h,element);
     }
     public static void main(String[] args) {
         long[] arr = {1, 2, 8, 10, 11, 12, 19};
-
         long[] testCase = {0,1,2,10,19,13,8};
 
         for (long l : testCase) {
