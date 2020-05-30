65d9e93d9745ce8a1269cd3612d31c0da1abd9ea
Vishal Singh <v.singh.kld@gmail.com>
2020-05-30 15:09:07 +0530

    FindFloor

---
+++ Searching/FindFloor.java
@@ -0,0 +1,41 @@
+package SummerTrainingGFG.Searching;
+/**
+ * @author Vishal Singh */
+public class FindFloor {
+    static int findFloor(long[] arr,int n,long element){
+        return find(arr,0,n-1,element);
+    }
+    static int find(long[] arr,int l,int h,long element){
+        if (l>h){
+            return -1;
+        }
+        int mid = l + (h-l)/2;
+        if (arr[mid] == element){
+            return mid;
+        }
+        if (mid > 0 && arr[mid-1] <= element && arr[mid] > element ){
+            return mid-1;
+        }
+        if (arr[mid] > element){
+            return find(arr,l,mid-1,element);
+        }
+        if (arr[mid] < element){
+            return find(arr,mid+1,h,element);
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        long[] arr = {1, 2, 8, 10, 11, 12, 19};
+
+        long[] testCase = {0,1,2,10,19,13,8};
+
+        for (long l : testCase) {
+            int index = findFloor(arr, arr.length, l);
+            if (index == -1) {
+                System.out.println("No such element");
+                continue;
+            }
+            System.out.println("Floor of "+l+" at "+"Index: " + index + " Element: " + arr[index]);
+        }
+    }
+}
