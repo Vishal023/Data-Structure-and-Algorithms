43dd5c586814db9a7bd3e2184e542a9d39876949
Vishal Singh <v.singh.kld@gmail.com>
2020-06-03 09:41:47 +0530

    Segregate Positive negative

---
+++ Sorting/SegregateElements.java
@@ -0,0 +1,44 @@
+package SummerTrainingGFG.Sorting;
+
+import java.util.Arrays;
+
+/**
+ * @author Vishal Singh
+ * Segregate +ve and -ve
+ * Segregate even and odd
+ * Segregate binary array
+ *      Method 1: By Hoare
+ *      Method 2: By Lomuto
+ * */
+public class SegregateElements {
+    static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    /**
+     * Used Hoare Partition*/
+    static void segregatePositiveNegative(int[] arr, int n){
+        int i = -1;
+        int j = n;
+        while (true){
+            do {
+                i++;
+            }while (arr[i]<0);
+            do {
+                j--;
+            }while (arr[j]>=0);
+            if (i>=j){
+                return;
+            }
+            swap(arr,i,j);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {-10,-20,1,2,-9,5,6};
+        segregatePositiveNegative(arr,arr.length);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
