b91f1f373cd732c1bc7c68224cd427c410155923
Vishal Singh <v.singh.kld@gmail.com>
2020-06-03 09:55:07 +0530

    Segregate Even Odd

--- Sorting/SegregateElements.java
+++ Sorting/SegregateElements.java
@@ -34,11 +34,29 @@ public class SegregateElements {
             swap(arr,i,j);
         }
     }
+    static void segregateEvenOdd(int[] arr,int n){
+        int i = -1;
+        int j = n;
+        while (true){
+            do {
+                i++;
+            }while (arr[i]%2 == 0 );
+            do {
+                j--;
+            }while (arr[j]%2 !=0 );
+            if (i>=j)
+                return;
+            swap(arr,i,j);
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {-10,-20,1,2,-9,5,6};
         segregatePositiveNegative(arr,arr.length);
         System.out.println(Arrays.toString(arr));
 
+        int[] arr1 = {1,10,3,8,0,2,5,6,7,9,13};
+        segregateEvenOdd(arr1,arr1.length);
+        System.out.println(Arrays.toString(arr1));
 
     }
 }
