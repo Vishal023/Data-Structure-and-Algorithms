d65cf6a24ec5a02432cf71d73d0b8af30e512e32
Vishal Singh <v.singh.kld@gmail.com>
2020-06-08 15:44:51 +0530

    Left most repeating

---
+++ Strings/LeftMostRepeating.java
@@ -0,0 +1,30 @@
+package SummerTrainingGFG.Strings;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @author Vishal Singh
+ */
+public class LeftMostRepeating {
+    static int leftmostRepeating(String str,int n){
+        int res = Integer.MAX_VALUE;
+        int[] firstIndex = new int[256];
+        for (int i = 0; i < 256; i++) {
+            firstIndex[i] = -1;
+        }
+        for (int i = 0; i < n; i++) {
+            if (firstIndex[str.charAt(i)] == -1){
+                firstIndex[str.charAt(i)] = i;
+            }
+            else {
+                res = Math.min(res,firstIndex[str.charAt(i)]);
+            }
+        }
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+    public static void main(String[] args) {
+        String str = "1-malayalam";
+        System.out.println(leftmostRepeating(str,str.length()));
+    }
+}
