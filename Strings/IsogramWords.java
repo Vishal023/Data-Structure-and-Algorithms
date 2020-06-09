6e36e071f56e7cd57afbba4982f6d1cd6f7e49cf
Vishal Singh <v.singh.kld@gmail.com>
2020-06-09 19:45:10 +0530

    Isogram Words

---
+++ Strings/IsogramWords.java
@@ -0,0 +1,29 @@
+package SummerTrainingGFG.Strings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Vishal Singh
+ */
+public class IsogramWords {
+    static void checkIsogramWords(String s){
+        int[] arr = new int[256];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            arr[s.charAt(i)]++;
+        }
+        for (int i = 0; i < 256; i++) {
+            if (arr[i]>1){
+                System.out.println("No it is not an Isogram");
+                return;
+            }
+        }
+        System.out.println("Yes it is an Isogram");
+    }
+    public static void main(String[] args)throws IOException {
+        String s = "Isogram";
+        checkIsogramWords(s);
+    }
+}
