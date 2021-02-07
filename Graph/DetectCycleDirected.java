61f7a90b8819dc417d9c202230c942f40bd29e48
Vishal Singh <42888189+Vishal023@users.noreply.github.com>
2021-02-07 11:22:25 +0530

    updated graph

---
+++ Graph/DetectCycleDirected.java
@@ -0,0 +1,62 @@
+package SummerTrainingGFG.Graph;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/**
+ * @author Vishal Singh
+ * 07-02-2021
+ */
+
+public class DetectCycleDirected {
+    public static void addEdge(ArrayList<ArrayList<Integer>> graph,int s,int d ){
+        graph.get(s).add(d);
+    }
+    public static void detectCycle(ArrayList<ArrayList<Integer>> graph,int v){
+        boolean[] vis = new boolean[v];
+        boolean[] recStack = new boolean[v];
+        boolean isCycle = false;
+        for (int i = 0; i < v; i++) {
+            if (!vis[i]){
+                if (dfs(graph,vis,recStack,i)){
+                    System.out.println("Cycle");
+                    isCycle = true;
+                }
+            }
+        }
+        if (!isCycle){
+            System.out.println("No cycle");
+        }
+    }
+    public static boolean dfs(ArrayList<ArrayList<Integer>> graph,boolean[] vis,boolean[] recStack,int s){
+        vis[s] = true;
+        recStack[s] = true;
+        for (int d: graph.get(s)){
+            if (!vis[d]){
+                if (dfs(graph,vis,recStack,d)){
+                    return true;
+                }
+            }else if (recStack[d]){
+                return true;
+            }
+        }
+        recStack[s] = false;
+        return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        int v = 6;
+        for (int i = 0; i < v; i++) {
+            graph.add(new ArrayList<>());
+        }
+        addEdge(graph,0,1);
+        addEdge(graph,2,1);
+        addEdge(graph,2,3);
+        addEdge(graph,3,4);
+        addEdge(graph,4,5);
+        addEdge(graph,5,3);
+        System.out.println(graph);
+        detectCycle(graph,v);
+    }
+}
---
+++ Graph/DetectCycleUndirected.java
@@ -0,0 +1,58 @@
+package SummerTrainingGFG.Graph;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/**
+ * @author Vishal Singh
+ * 06-02-2021
+ */
+
+public class DetectCycleUndirected {
+    private static void addEdge(ArrayList<LinkedList<Integer>> graph,int u,int v){
+        graph.get(u).add(v);
+        graph.get(v).add(u);
+    }
+    private static boolean dfs(ArrayList<LinkedList<Integer>> graph,int s,boolean[] visited,int parent){
+        visited[s] = true;
+        for (int v: graph.get(s)){
+            if (!visited[v]){
+                if (dfs(graph,v,visited,s)){
+                    return true;
+                }
+            }else if (v!=parent){
+                return true;
+            }
+        }
+        return false;
+    }
+    private static void detectCycle(ArrayList<LinkedList<Integer>> graph,int v){
+        /* Modified DFS */
+        boolean[] visited = new boolean[v];
+        boolean isCycle = false;
+        for (int i = 0; i < v; i++) {
+            if (!visited[i]){
+                if (dfs(graph,i,visited,-1)){
+                    System.out.println("Cycle Present");
+                    isCycle = true;
+                    break;
+                }
+            }
+        }
+        if (!isCycle){
+            System.out.println("No cycle present");
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<LinkedList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            graph.add(new LinkedList<>());
+        }
+        addEdge(graph,0,1);
+        addEdge(graph,1,2);
+        addEdge(graph,2,3);
+        addEdge(graph,0,2);
+        detectCycle(graph,4);
+    }
+}
